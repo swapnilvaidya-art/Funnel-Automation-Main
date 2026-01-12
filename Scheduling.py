73277471e278da08c4081c65c044639e215c94eb
swapnilvaidya-art <swapnil.vaidya@newtonschool.co>
2026-01-12 16:00:40 +0530

    Update Scheduling.py
    print(results)

--- Scheduling.py
+++ Scheduling.py
@@ -99,6 +99,7 @@ with ThreadPoolExecutor(max_workers=2) as executor:
     futures = {name: executor.submit(fetch_with_retry, url, METABASE_HEADERS) for name, url in urls.items()}
     results = {name: f.result() for name, f in futures.items()}
 
+print(results)
 df_Funnel = pd.DataFrame(results["Funnel"].json())
 df_Createon = pd.DataFrame(results["Createdon"].json())
 
